deff08539f48fbf17d4ccd06293ffa73b58d2269
Dhanish Siddharth <78961654+dxn1310@users.noreply.github.com>
2023-07-18 19:32:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,32 +5,32 @@ import cv2
 import tensorflow as tf
 from PIL import Image
 
-model = pickle.load(open('digitrecognizer.pkl', 'rb')) #using the saved model for predicting new handdrawn digit
+# model = pickle.load(open('digitrecognizer.pkl', 'rb')) #using the saved model for predicting new handdrawn digit
 
 st.markdown("<h1 style='text-align: center; color: white;'>Handwritten Digit Identification</h1>", unsafe_allow_html=True)
 # st.write(""" ##### Upload an image and please wait for a few seconds """)
 
 uploaded_file = st.file_uploader("Upload an image",type=("png"))
 
-if uploaded_file != None:
-    image = Image.open(uploaded_file)
-    img = np.array(image)
+# if uploaded_file != None:
+#     image = Image.open(uploaded_file)
+#     img = np.array(image)
 
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    resized = cv2.resize(gray,(28,28),interpolation = cv2.INTER_AREA)
+#     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#     resized = cv2.resize(gray,(28,28),interpolation = cv2.INTER_AREA)
 
-    newimg = tf.keras.utils.normalize(resized,axis=1)
-    newimg = np.array(newimg).reshape(-1,28,28,1)
+#     newimg = tf.keras.utils.normalize(resized,axis=1)
+#     newimg = np.array(newimg).reshape(-1,28,28,1)
 
-    predictions = model.predict(newimg) #predicting the hand drawn digit using model
+#     predictions = model.predict(newimg) #predicting the hand drawn digit using model
   
 
-    c1, c2 = st.columns(2)
+#     c1, c2 = st.columns(2)
 
-    with c1:
-        st.image(uploaded_file,caption='The uploaded image of digit',width=200)
-    with c2:
-        st.write(f"### The digit identified is : {np.argmax(predictions)}")
+#     with c1:
+#         st.image(uploaded_file,caption='The uploaded image of digit',width=200)
+#     with c2:
+#         st.write(f"### The digit identified is : {np.argmax(predictions)}")
 
-else:
-    st.write(""" ##### Upload an image and please wait for a few seconds """)
+# else:
+#     st.write(""" ##### Upload an image and please wait for a few seconds """)
